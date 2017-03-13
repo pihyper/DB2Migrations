6e797a184a9b36227cb69bb22975280901fcb8e9
d3vr <d3vr@mitidji.com>
2017-03-13 23:19:18 +0100

    Fixed a minor bug in the service provider

--- src/d3vr/DB2Migrations/DB2MigrationsServiceProvider.php
+++ src/d3vr/DB2Migrations/DB2MigrationsServiceProvider.php
@@ -28,7 +28,7 @@ class DB2MigrationsServiceProvider extends ServiceProvider {
     public function register()
     {
         $this->app->singleton('artisan.convert.migrations', function($app) {
-            return new ConvertMigrationsCommand;
+            return new DB2MigrationsCommand;
         });
 
         $this->commands('artisan.convert.migrations');
